89f1fc8aaf410466495b482e4727cd52c9c3f871
Sapnapalkhade <sapnapalkhade@gmail.com>
2023-09-15 16:05:41 -0700

    capstone-project

---
+++ server.js
@@ -0,0 +1,12 @@
+require('dotenv').config()
+const app = require('./app-server')
+const mongoose = require('mongoose')
+const PORT = process.env.PORT || 3000
+
+mongoose.connect(process.env.MONGO_URI)
+console.log("******MONGO_URI*****",process.env.MONGO_URI)
+mongoose.connection.once('open', () => console.log('MongoDB is connected successfully..YAY!'))
+
+app.listen(PORT, () => {
+    console.log(`Node server is running on port ${PORT}`)
+})
