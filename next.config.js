7ef165b140398b956a6609b399bb026e7a78f4ac
Sapnapalkhade <sapnapalkhade@gmail.com>
2023-09-22 19:56:38 -0700

    another

--- next.config.js
+++ next.config.js
@@ -1,22 +1,22 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {}
+/** @type {import('next').NextConfig} */
+const nextConfig = {}
 
-// module.exports = nextConfig
-const path = require('path');
+module.exports = nextConfig
+// const path = require('path');
 
-module.exports = {
-  webpack(config) {
-    config.resolve.alias['~'] = path.resolve(__dirname, 'src');
-    return config;
-  },
-  //distDir: 'build', // Specify the build folder here
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/LoginPage/LoginPage',
-        permanent: true,
-      },
-    ];
-  },
-};
+// module.exports = {
+//   webpack(config) {
+//     config.resolve.alias['~'] = path.resolve(__dirname, 'src');
+//     return config;
+//   },
+//   //distDir: 'build', // Specify the build folder here
+//   async redirects() {
+//     return [
+//       {
+//         source: '/',
+//         destination: '/LoginPage/LoginPage',
+//         permanent: true,
+//       },
+//     ];
+//   },
+// };
